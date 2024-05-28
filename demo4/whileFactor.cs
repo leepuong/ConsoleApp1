fabed9a2a4bacf6df332cfae4969531e545973bf
leepuong <hhyfuihggtuucg@gmail.com>
2024-05-28 10:24:14 +0700

    add file whilefactor.cs

---
+++ demo4/whileFactor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    internal class whileFactor
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, s = 1, n;
+            n = int.Parse(Console.ReadLine());
+
+            while (i <= n)
+            {
+                s = s * i;
+                i++;
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
