4a4599fabf174b1e303e63b74e2e29ddef369528
leepuong <hhyfuihggtuucg@gmail.com>
2024-05-28 10:12:13 +0700

    add file while.cs

---
+++ demo4/while.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    internal class @while
+    {
+        static void Main(string[] args)
+        {
+            int n, i = 1;
+            n = int.Parse(Console.ReadLine());
+            while (i<=10)
+            {
+                Console.WriteLine("{0} * {1} = {2}",n,i,n*i);
+                i++;
+            }
+            Console.ReadKey();  
+        }
+    }
+}
