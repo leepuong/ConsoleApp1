a549a0ca72236758ee29faa00ae8a0b9fc1ee297
leepuong <hhyfuihggtuucg@gmail.com>
2024-05-28 16:18:38 +0700

    ad lap2-ex2

---
+++ homework/lap2_ex2_multitable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class lap2_ex2_multitable
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("input n :: ");
+            n = int.Parse(Console.ReadLine()); 
+            if (n <=100 && n >= 0)
+            {
+                for (i = 1; i <= 10; i++)
+                {
+                Console.WriteLine("{0:d2} * {1:d2} = {2:d2}",n, i, n*i);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("n is wrong");
+            }
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
