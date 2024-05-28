5ecfa612b70d9a531f04fba868b368b2b25d150a
leepuong <hhyfuihggtuucg@gmail.com>
2024-05-28 11:10:57 +0700

    add file matrix and dowhilepass

---
+++ demo4/dowhile.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    internal class dowhile
+    {
+        static void Main(string[] args)
+        {
+            int pass = 108526, p,i=0;
+            do
+            {
+                Console.WriteLine("enter pass :: ");
+                p = int.Parse(Console.ReadLine());
+                i++;
+                if (i >= 3)
+                {
+                    break;
+                }
+                
+                
+            }
+            while( p != pass );
+            if (i >= 3)
+                {
+                    Console.WriteLine("wrong the pass");
+                    Console.ReadKey();
+                    
+                }
+            else
+            {
+                Console.WriteLine("yaa~~");
+
+            }
+            Console.ReadKey();
+
+
+
+
+            
+        }
+    }
+}
---
+++ demo4/drawMatrix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    internal class drawMatrix
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("so hang : ");
+            int h = int.Parse(Console.ReadLine());
+            Console.Write("so cot : ");
+            int c = int.Parse(Console.ReadLine());
+
+            for (int hang = 1; hang <= h; hang++)
+            {
+                for (int cot = 1; cot <= c; cot++)
+                {
+                    Console.Write("|A");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
