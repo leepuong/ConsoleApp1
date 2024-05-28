00def19be916366be3499198497cc49861739f7f
leepuong <hhyfuihggtuucg@gmail.com>
2024-05-28 10:19:31 +0700

    add file whilesumsquares.cs

---
+++ demo4/whileSumSquares.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4
+{
+    internal class whileSumSquares
+    {   
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 0, s = 0;
+
+            while ( i <= n)
+            {
+                s = s + (i * i);
+                i++;
+            }
+            Console.WriteLine("yaa~~~" + s);
+            Console.ReadLine();
+        }
+    }
+}
